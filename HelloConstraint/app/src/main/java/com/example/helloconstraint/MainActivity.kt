f9962b25c4db9880b478c2b4af58b2633d17521d
YRT9GA <yrt12345@gmail.com>
2022-01-22 14:01:12 +0300

    Added extensions in HelloConstraint & HelloToast

--- HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.kt
+++ HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import helloconstraint.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,38 +16,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mShowCount = findViewById<TextView>(R.id.show_count)
+        val zeroBtn = findViewById<Button>(R.id.button_to_zero)
+        val countBtn = findViewById<Button>(R.id.button_count)
 
-
+        button_toast.setOnClickListener {
+            val toast = Toast.makeText(
+                this, R.string.toast_message,
+                Toast.LENGTH_SHORT
+            )
+            toast.show()
         }
 
+        button_count.setOnClickListener {
+            countBtn.setBackgroundColor(Color.GREEN)
+            mCount++
+            mShowCount?.setText(mCount.toString())
+            zeroBtn.setBackgroundColor(Color.RED)
+        }
 
-
-    fun countUp(view: android.view.View) {
-        val mShowCount = findViewById<TextView>(R.id.show_count)
-        val zeroBtn = findViewById<Button>(R.id.zeroButton)
-        val countBtn = findViewById<Button>(R.id.button_count)
-        countBtn.setBackgroundColor(Color.GREEN)
-        mCount++
-        mShowCount?.setText(mCount.toString())
-        zeroBtn.setBackgroundColor(Color.RED)
-
+        button_to_zero.setOnClickListener {
+            countBtn.setBackgroundColor(Color.BLUE)
+            zeroBtn.setBackgroundColor(Color.GRAY)
+            mCount = 0
+            mShowCount.setText(mCount.toString())
+        }
     }
 
-    fun showToast(view: android.view.View) {val toast  = Toast.makeText(this, R.string.toast_message,
-        Toast.LENGTH_SHORT)
-        toast.show()
-    }
-
-    fun countZero(view: android.view.View) {
-        val mShowCount = findViewById<TextView>(R.id.show_count)
-        val zeroBtn = findViewById<Button>(R.id.zeroButton)
-        val countBtn = findViewById<Button>(R.id.button_count)
-        countBtn.setBackgroundColor(Color.BLUE)
-        zeroBtn.setBackgroundColor(Color.GRAY)
-        mCount = 0
-        mShowCount.setText(mCount.toString())
-
-    }
 
 }
 
