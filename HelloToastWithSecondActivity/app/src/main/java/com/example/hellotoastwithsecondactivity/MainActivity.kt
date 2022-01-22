0573eb53f855ac76216e2cd38295a9df0e8ed207
YRT9GA <yrt12345@gmail.com>
2022-01-22 14:06:27 +0300

    Added extensions in HelloToastWithSecondActivity

--- HelloToastWithSecondActivity/app/src/main/java/com/example/hellotoastwithsecondactivity/MainActivity.kt
+++ HelloToastWithSecondActivity/app/src/main/java/com/example/hellotoastwithsecondactivity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,26 +14,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-    }
-
-
-    fun countUp(view: android.view.View) {
-        mCount++
         val mShowCount = findViewById<TextView>(R.id.show_count)
-        if (mShowCount != null)
-            mShowCount.setText(mCount.toString())
+
+        button_count.setOnClickListener {
+            mCount++
+            if (mShowCount != null)
+                mShowCount.setText(mCount.toString())
+        }
+
+        button_hello.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            val message = mShowCount.text.toString()
+            intent.putExtra(EXTRA_MESSAGE, message)
+            startActivity(intent)
+        }
+
     }
 
 
-    fun sayHello(view: android.view.View) {
-        val mShowCount = findViewById<TextView>(R.id.show_count)
-        val intent = Intent(this, SecondActivity::class.java)
-        val message = mShowCount.text.toString()
-        intent.putExtra(EXTRA_MESSAGE, message)
-        startActivity(intent)
-    }
+//    fun countUp(view: android.view.View) {
+//        mCount++
+//        val mShowCount = findViewById<TextView>(R.id.show_count)
+//        if (mShowCount != null)
+//            mShowCount.setText(mCount.toString())
+//    }
+//
+//
+//    fun sayHello(view: android.view.View) {
+//        val mShowCount = findViewById<TextView>(R.id.show_count)
+//        val intent = Intent(this, SecondActivity::class.java)
+//        val message = mShowCount.text.toString()
+//        intent.putExtra(EXTRA_MESSAGE, message)
+//        startActivity(intent)
+//    }
 
     companion object {
 
